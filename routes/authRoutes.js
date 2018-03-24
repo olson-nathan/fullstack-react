af8f2e8d61c28a092cd404b4849219ec19068ee2
Nathan Olson <nolson@nvenergy.com>
2018-03-23 23:07:15 -0700

    Add console output for api/current_user

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -11,6 +11,7 @@ module.exports = (app) => {
         res.send(req.user);
     });
     app.get('/api/current_user', (req, res) => {
+        console.log(req.user);
         res.send(req.user);
     });
 }
